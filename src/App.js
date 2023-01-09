57f398912300a4e6d0178f2da2083cc52b9e2df6
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-01-08 23:10:49 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
     <>
-      <div className="m-3 ">
-
-      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+       </Routes>
     </>
   );
 }
