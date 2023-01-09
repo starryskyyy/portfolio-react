dce0fab38a7521b86a4acb840e182e7b8da345b6
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-01-08 21:22:00 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-1': '#01080E',
+        'primary-2': '#011627', 
+        'primary-3': '#011221',
+        'secondary-1': '#607B96',
+        'secondary-2': '#3C9D93',
+        'secondary-3': '#4D5BCE',
+        'accent-1': '#FEA55F',
+        'accent-2': '#43D9AD',
+        'accent-3': '#E99287',
+        'accent-4': '#C98BDF',
+        'lines': '#1E2D3D',
+      },
+    },
   },
   plugins: [],
 }
