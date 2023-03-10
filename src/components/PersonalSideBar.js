44cf5fea84811e49c9cc9d0108847799caa21090
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-09 20:44:49 -0500

    added navigation to a different components onclick

--- src/components/PersonalSideBar.js
+++ src/components/PersonalSideBar.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import * as Accordion from '@radix-ui/react-accordion';
 import { TriangleRightIcon } from '@radix-ui/react-icons';
+import AboutText from './AboutText'
+import Interests from './Interests';
+import Education from './Education';
 
 export default function PersonalSideBar() {
     const [active, setActive] = useState(1);
@@ -26,57 +29,26 @@ export default function PersonalSideBar() {
         },
     ];
 
-
+    const handleItemClick = (pageName, itemId) => {
+        setActive(itemId);
+      };
+    
+      let activeComponent;
+      switch (active) {
+        case 1:
+          activeComponent = <AboutText/>;
+          break;
+        case 2:
+          activeComponent = <Interests/>;
+          break;
+        case 3:
+          activeComponent = <Education/>;
+          break;
+        default:
+          activeComponent = null;
+      }
     return (
-        /*
-        <div class="w-60 h-full " id="sidenavExample">
-            <Accordion.Root class="text-white" type="multiple" collapsible defaultValue={["item-1"]}>
-                <Accordion.Item value="item-1">
-                    <Accordion.Header class="border-solid border-b border-lines p-2">
-                        <Accordion.Trigger className="AccordionTrigger">
-                            <TriangleRightIcon className="AccordionChevron" aria-hidden />
-                            <span class="ml-4">personal-info</span>
-                        </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content className="AccordionContent">
-                        {values.map((val) => (
-                            <a href="#" onClick={() => setActive(val.id)}>
-                                <span class="icon pr-2">
-                                    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d={val.svgPath} fill={val.color} /></svg>
-                                </span>
-                                <span  class={active === val.id ? "text-white" : "text-secondary-1"}>{val.name}</span>
-                                <br/><br/>
-                            </a>
-                        ))}
-                    </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item value="item-2">
-                    <Accordion.Header class="border-solid border-y border-lines p-2">
-                        <Accordion.Trigger className="AccordionTrigger">
-                            <TriangleRightIcon className="AccordionChevron" aria-hidden />
-                            <span class="ml-4">contacts</span>
-
-                        </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content className="AccordionContent" class="py-6 pl-3">
-                        <span>
-                            <span class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm9.06 8.683L5.648 6.238 4.353 7.762l7.72 6.555 7.581-6.56-1.308-1.513-6.285 5.439z" fill="rgba(96,123,150,1)" /></svg>
-                            </span>
-                            <span class="text-sm pl-2 text-secondary-1">vygovskaia.e@gmail.com</span>
-                        </span>
-                        <br />
-                        <span class="mt-6">
-                            <span class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d="M21 16.42v3.536a1 1 0 0 1-.93.998c-.437.03-.794.046-1.07.046-8.837 0-16-7.163-16-16 0-.276.015-.633.046-1.07A1 1 0 0 1 4.044 3H7.58a.5.5 0 0 1 .498.45c.023.23.044.413.064.552A13.901 13.901 0 0 0 9.35 8.003c.095.2.033.439-.147.567l-2.158 1.542a13.047 13.047 0 0 0 6.844 6.844l1.54-2.154a.462.462 0 0 1 .573-.149 13.901 13.901 0 0 0 4 1.205c.139.02.322.042.55.064a.5.5 0 0 1 .449.498z" fill="rgba(96,123,150,1)" /></svg>
-                            </span>
-                            <span class="text-sm pl-2 text-secondary-1">+1 905 226-95-25</span>
-                        </span>
-                    </Accordion.Content>
-                </Accordion.Item>
-            </Accordion.Root>
-        </div>
-        */
+        
         <div div class="h-full">
             <div class="h-full flex flex-row ...">
             <div class="w-60 h-full border-solid border-r border-lines" id="sidenavExample">
@@ -90,7 +62,7 @@ export default function PersonalSideBar() {
                     </Accordion.Header>
                     <Accordion.Content className="AccordionContent">
                         {values.map((val) => (
-                            <a href="#" onClick={() => setActive(val.id)}>
+                            <a href="#" onClick={() => handleItemClick(val.path, val.id)}>
                                 <span class="icon pr-2">
                                     <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d={val.svgPath} fill={val.color} /></svg>
                                 </span>
@@ -126,7 +98,7 @@ export default function PersonalSideBar() {
                 </Accordion.Item>
             </Accordion.Root>
         </div>
-                <div className="p-10 h-full w-45% border-solid border-r border-lines">02</div>
+                <div className="h-full w-1/2 border-solid border-r border-lines">{activeComponent}</div>
                 <div className="p-10">03</div>
             </div>
         </div>
