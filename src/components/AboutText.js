236c76653b287b3dc805115ef44dafb6ec127e4b
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-09 19:04:03 -0500

    added about text

---
+++ src/components/AboutText.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function AboutText() {
+  return (
+    <div className="text-secondary-1 text-base">1 &nbsp;&nbsp;/*<br></br>
+    2     &nbsp;&nbsp;* About me<br></br>
+    3     &nbsp;&nbsp;*<br></br>
+    4     &nbsp;&nbsp;* I am a current student at George Brown College<br></br>
+    5     &nbsp;&nbsp;* studying Computer Programming and Analysis.<br></br>
+    6     &nbsp;&nbsp;*<br></br>
+    7     &nbsp;&nbsp;* As a passionate and motivated professional,<br></br>
+    8     &nbsp;&nbsp;* I’m constantly striving to improve my techniques,<br></br>
+    9     &nbsp;&nbsp;* expand my skillset and find new opportunities to grow.<br></br>
+    10    &nbsp;* Each of my projects - both solo and collaborative - have<br></br>
+    11    &nbsp;* provided this growth and allowed me to establish<br></br>
+    12    &nbsp;* myself within this competitive industry.<br></br>
+    13    &nbsp;*<br></br>
+    14    &nbsp;* Please enjoy my portfolio, and feel free to get<br></br>
+    15    &nbsp;* in touch with me if you have any questions.<br></br>
+    16    &nbsp;*/</div>
+  )
+}
