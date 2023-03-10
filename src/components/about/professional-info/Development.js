704389dcc300afbf6fda60c9a3cb6c6195d635ac
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-10 18:49:07 -0500

    Update Development.js

--- src/components/about/professional-info/Development.js
+++ src/components/about/professional-info/Development.js
@@ -43,7 +43,7 @@ export default function Development() {
             <p className="pb-0.5 text-white">Java</p>
           </div>
           <p className=" text-accent-2">{'>'} 2 years</p>
-          <p>I have gained proficiency in application development by working on multiple projects, utilizing Data Structures and Algorithms in Java. Through experimenting with various amounts of data, I have analyzed the efficiency of different algorithms, which in turn helped me optimize my applications. Furthermore, I have expanded my skillset by learning the Spring Boot framework while developing a web application.</p>
+          <p>I have gained proficiency in application development by working on multiple projects, utilizing Data Structures and Algorithms in Java. Through experimenting with various amounts of data, I have analyzed the efficiency of different algorithms, which in turn helped me optimize my applications. Furthermore, I have expanded my skillset by learning the Spring Boot framework while developing a web application. Have practical knowledge of building an Android applications.</p>
           <br></br>
           <div className="flex items-center">
             <svg
@@ -70,6 +70,19 @@ export default function Development() {
           </div>
           <p className=" text-accent-2">{'>'} 1 year</p>
           <p>I have experience working with different databases for various applications. SQL and SQLite were used for console and GUI applications created with C#, Java, and Python, while MongoDB and GraphQL were used for web applications. This experience has given me a comprehensive understanding of database design and management, allowing me to optimize application performance and functionality.</p>
+          <br></br>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">Swift</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 1 year</p>
+          <p>I have gained experience in developing iOS applications using Swift, which I learned through a Mobile Application class. During the course, I built a To-Do application using UIKit for the frontend, which helped me to develop my proficiency in Swift and iOS app development.</p>
           <br></br>
         </div>
       </div>
