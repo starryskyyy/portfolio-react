a47fce8edd6fe6bae25b63ef7905ddf4cd39333e
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-10 16:11:28 -0500

    added development component

---
+++ src/components/about/professional-info/Development.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export default function Development() {
+  return (
+    <div className=" w-full h-full">
+      <div className="border-solid border-b border-lines p-5"></div>
+      <div className="h-180 overflow-auto crollbar-w-2 scrollbar-track-gray-200 scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full ">
+        <div className="text-secondary-1 text-base p-10">
+        <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">JavaScript</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 2 years</p>
+          <p>I have extensive experience in building diverse web applications using JavaScript and its various frameworks. Specifically, I have developed a variety of applications, including a weather app, an employee management system, and a real-time chat web application using Socket.io, which has strengthened my proficiency in developing interactive web applications. I utilized React for the frontend and Express for the backend to build RESTful APIs in these applications.</p>
+          <br></br>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">C#</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 2 years</p>
+          <p>I have experience working on both individual and team projects, where I have developed console and graphical user interface applications utilizing Object Oriented Programming principles. Additionally, I have created web applications using ASP.Net C# framework.</p>
+          <br></br>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">Java</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 2 years</p>
+          <p>I have gained proficiency in application development by working on multiple projects, utilizing Data Structures and Algorithms in Java. Through experimenting with various amounts of data, I have analyzed the efficiency of different algorithms, which in turn helped me optimize my applications. Furthermore, I have expanded my skillset by learning the Spring Boot framework while developing a web application.</p>
+          <br></br>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">Databases</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 2 year</p>
+          <p>By creating console applications, such as a library system with a connected database, I have gained a strong foundational understanding of Python. Furthermore, I have extended my proficiency by taking classes in Data Science and Machine Learning, using Python and its associated libraries. Through these classes, I have applied my programming knowledge to develop complex algorithms and predictive models, which have contributed to my comprehensive understanding of Python's capabilities.</p>
+          <br></br>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              className="mr-2"><path
+                fill="none" d="M0 0h24v24H0z" /><path d="M5.33 3.271a3.5 3.5 0 0 1 4.472 4.474L20.647 18.59l-2.122 2.121L7.68 9.867a3.5 3.5 0 0 1-4.472-4.474L5.444 7.63a1.5 1.5 0 1 0 2.121-2.121L5.329 3.27zm10.367 1.884l3.182-1.768 1.414 1.414-1.768 3.182-1.768.354-2.12 2.121-1.415-1.414 2.121-2.121.354-1.768zm-7.071 7.778l2.121 2.122-4.95 4.95A1.5 1.5 0 0 1 3.58 17.99l.097-.107 4.95-4.95z" fill="rgba(126,145,165,1)" /></svg>
+            <p className="pb-0.5 text-white">Python</p>
+          </div>
+          <p className=" text-accent-2">{'>'} 1 year</p>
+          <p>I have experience working with different databases for various applications. SQL and SQLite were used for console and GUI applications created with C#, Java, and Python, while MongoDB and GraphQL were used for web applications. This experience has given me a comprehensive understanding of database design and management, allowing me to optimize application performance and functionality.</p>
+          <br></br>
+        </div>
+      </div>
+    </div>
+  )
+}
