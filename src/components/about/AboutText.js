970ae462e9270c756fa47046eb8851e5a93a2ef8
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-11 18:55:37 -0500

    rearranged

--- src/components/about/AboutText.js
+++ src/components/about/AboutText.js
@@ -2,23 +2,28 @@ import React from 'react'
 
 export default function AboutText() {
   return (
-    <div className="h-full w-full">
+    <div className="w-full">
       <div className="border-solid border-b border-lines p-5"></div>
-      <div className="text-secondary-1 text-base p-14">1 &nbsp;&nbsp;/*<br></br>
-        2     &nbsp;&nbsp;* About me<br></br>
-        3     &nbsp;&nbsp;*<br></br>
-        4     &nbsp;&nbsp;* I am a current student at George Brown College<br></br>
-        5     &nbsp;&nbsp;* studying Computer Programming and Analysis.<br></br>
-        6     &nbsp;&nbsp;*<br></br>
-        7     &nbsp;&nbsp;* As a passionate and motivated professional,<br></br>
-        8     &nbsp;&nbsp;* I’m constantly striving to improve my techniques,<br></br>
-        9     &nbsp;&nbsp;* expand my skillset and find new opportunities to grow.<br></br>
-        10    &nbsp;* Each of my projects - both solo and collaborative - have<br></br>
-        11    &nbsp;* provided this growth and allowed me to establish<br></br>
-        12    &nbsp;* myself within this competitive industry.<br></br>
-        13    &nbsp;*<br></br>
-        14    &nbsp;* Please enjoy my portfolio, and feel free to get<br></br>
-        15    &nbsp;* in touch with me if you have any questions.<br></br>
-        16    &nbsp;*/</div></div>
+      <div className="h-180 overflow-auto scrollbar-w-2 scrollbar-track-gray-200 scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full ">
+        <div className="text-secondary-1 text-base p-14">1 &nbsp;&nbsp;/*<br></br>
+          2     &nbsp;&nbsp;* About me<br></br>
+          3     &nbsp;&nbsp;*<br></br>
+          4     &nbsp;&nbsp;* I am a current student at George Brown College<br></br>
+          5     &nbsp;&nbsp;* studying Computer Programming and Analysis.<br></br>
+          6     &nbsp;&nbsp;*<br></br>
+          7     &nbsp;&nbsp;* As a passionate and motivated professional,<br></br>
+          8     &nbsp;&nbsp;* I’m constantly striving to improve my techniques,<br></br>
+          9     &nbsp;&nbsp;* expand my skillset and find new opportunities to grow.<br></br>
+          10    &nbsp;* Each of my projects - both solo and collaborative - have<br></br>
+          11    &nbsp;* provided this growth and allowed me to establish<br></br>
+          12    &nbsp;* myself within this competitive industry.<br></br>
+          13    &nbsp;*<br></br>
+          14    &nbsp;* Please enjoy my portfolio, and feel free to get<br></br>
+          15    &nbsp;* in touch with me if you have any questions.<br></br>
+          16    &nbsp;*/
+        </div>
+      </div>
+    </div>
   )
 }
+
