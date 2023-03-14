526564e1c1954ef6c992618cc5df807afb402d7b
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-13 22:17:09 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,10 +7,30 @@ export default function Contact() {
         <div className="w-full bg-primary-2 border-solid border border-lines rounded-md h-full">
             <div class="flex flex-col h-full justify-between">
                 <header class="h-auto"><NavBar /></header>
-                <main class="mb-auto">Contact</main>
+                <main class="mb-auto h-full w-full">
+                    <div className="flex flex-row h-full w-full">
+                        <div className="h-full w-1/5 border-solid border-r border-lines">
+                            <div className="border-solid border-b border-lines">
+                                <p className="p-2 text-white">contact-info</p>
+                            </div>
+                            <div className="p-6 text-secondary-1 text-sm">
+
+                            </div>
+                        </div>
+                        <div className=" h-full w-47 border-solid border-r border-lines">
+                            <div className="border-solid border-b border-lines">
+                                <p className="p-2 text-white">contact-form</p>
+                            </div>
+                        </div>
+                        <div className="h-full w-1/3 border-solid border-r border-lines">
+                            <div className="border-solid border-b border-lines">
+                                <p className="p-2 text-white">contact-code</p>
+                            </div>
+                        </div>
+                    </div>
+                </main>
                 <footer class="h-auto"><Footer /></footer>
             </div>
         </div>
-
     )
 }
