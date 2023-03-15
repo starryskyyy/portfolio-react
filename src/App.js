024325800c1e464db6c5e9d073170d3821089674
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-14 21:33:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/about/About';
 import Projects from './components/Projects';
@@ -9,17 +9,13 @@ import NoMatch from './components/NoMatch';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+       </Routes>
     </>
   );
 }
